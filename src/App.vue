f7afd359a1a35229951fdbbecedaf04e7bb0379a
@danilko.a.g <danilko.a.g@gmail.com>
2025-09-10 18:35:23 +0300

    remove base init components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
-
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-    <h1 class="text-4xl font-bold text-blue-600">
-      Vue 3 + Tailwind + TypeScript + Pinia 🚀
-    </h1>
-    <p class="mt-4 text-lg text-gray-700">Tailwind подключен успешно ✅</p>
-  </div>
+  <RouterView />
 </template>
